0f3f3c83b6b1b7cdc6d5b630ad5567412c092f1e
jpillet <jpillet@gmail.com>
2018-09-13 20:07:46 +0000

    automatically backed up by learn

---
+++ final-project/index.js
@@ -0,0 +1,15 @@
+document.querySelector(".active").addEventListener('click', () => {
+  document.getElementById('invitation').classList.add("invitation");
+});
+
+document.querySelector(".reactive").addEventListener('click', () => {
+  document.getElementById('invitation').classList.add("invitation2");
+});
+
+document.querySelector(".deactive").addEventListener('click', () => {
+  document.getElementById('invitation').classList.add("invitation3");
+});
+
+var x = document.getElementById("firstname").value
+$(":text")
+element.addEventListener("keydown", function(){ ); });
